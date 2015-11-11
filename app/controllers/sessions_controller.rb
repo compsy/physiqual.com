5a9185a5662d98191e2ee89412d3179b0df4e434
Ando Emerencia <ando.emerencia@gmail.com>
2015-11-11 14:50:02 +0100

    rubocop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,22 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
+  # rubocop:disable Metrics/AbcSize
   def create
     reset_session
     auth = request.env['omniauth.auth']
     Rails.logger.info "OmniAuth info: #{auth.to_yaml}"
 
-    session[:login]  = auth['extra']['raw_info']['login']
     session[:email]  = auth['extra']['raw_info']['email']
     session[:name]   = auth['extra']['raw_info']['name']
-    session[:avatar] = auth['extra']['raw_info']['avatar_url']
     session[:token]  = auth['credentials']['token']
 
     Physiqual::User.create(email: session[:email]) unless Physiqual::User.find_by_email(session[:email])
     redirect_to root_url, flash: { success: "Welcome #{session[:name]}!" }
   end
 
+  # rubocop:enable Metrics/AbcSize
   def failure
     redirect_to new_session_url,
                 flash: { error: 'Sorry, there was something wrong with your login attempt. Please try again.' }
