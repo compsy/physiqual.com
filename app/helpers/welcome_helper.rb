2c66129d454fc93d8031abbdbff1e1c99e1174bf
Ando Emerencia <ando.emerencia@gmail.com>
2016-01-13 11:25:27 +0100

    support adding one token only

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -9,18 +9,8 @@ module WelcomeHelper
     session[:physiqual_user_id] && current_user
   end
 
-  def number_of_tokens
+  def has_token?
     # precondition: user is logged in (logged_in?)
     current_user.physiqual_tokens.size
-  end
-
-  def fitbit_token?
-    # precondition: user is logged in (logged_in?)
-    current_user.fitbit_tokens.size > 0
-  end
-
-  def google_token?
-    # precondition: user is logged in (logged_in?)
-    current_user.google_tokens.size > 0
   end
 end
